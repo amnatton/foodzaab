640428f24bda35014420b083ddbbc5bcfb684cbe
amnatton <prueksa.soft@gmail.com>
2020-09-25 18:50:19 +0700

    Show Shop 25-09-2563

--- lib/widget/information_shop.dart
+++ lib/widget/information_shop.dart
@@ -1,6 +1,13 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:foodzaab/model/user_model.dart';
 import 'package:foodzaab/screens/add_info_shop.dart';
+import 'package:foodzaab/utility/my_constant.dart';
 import 'package:foodzaab/utility/my_style.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class InformationShop extends StatefulWidget {
   @override
@@ -8,6 +15,32 @@ class InformationShop extends StatefulWidget {
 }
 
 class _InformationShopState extends State<InformationShop> {
+  UserModel userModel;
+  @override
+  void initState() {
+    super.initState();
+    readDataUser();
+  }
+
+  Future<Null> readDataUser() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    String id = preferences.getString('id');
+    String url =
+        '${MyConstant().domain}/foodzaab/getUserWhereId.php?isAdd=true&id=$id';
+    await Dio().get(url).then((value) {
+      //print('value = $value');
+      var result = json.decode(value.data);
+      //print('result = $result');
+      for (var map in result) {
+        setState(() {
+          userModel = UserModel.fromJson(map);
+        });
+        print('nameShop = ${userModel.nameShop}');
+        if (userModel.nameShop.isEmpty) {}
+      }
+    });
+  }
+
   void routeToAddInfo() {
     print('routeToAddInfo Work');
     MaterialPageRoute materialPageRoute = MaterialPageRoute(
@@ -20,11 +53,67 @@ class _InformationShopState extends State<InformationShop> {
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
-        MyStyle()
-            .titleCenter(context, 'ยังไม่มีข้อมูล กรุณาเพิ่มข้อมูลด้วยค่ะ'),
+        userModel == null
+            ? MyStyle().showProgress()
+            : userModel.nameShop.isEmpty
+                ? showNoData(context)
+                : showListInfoShop(),
         addAnEditButton()
       ],
     );
+  }
+
+  Widget showListInfoShop() => Column(
+        children: <Widget>[
+          MyStyle().showTitleH2('ร้าน: ${userModel.nameShop}'),
+          Row(
+            children: [
+              MyStyle().showTitleH2('ที่อยู่: '),
+            ],
+          ),
+          Row(
+            children: [
+              Text(userModel.address),
+            ],
+          ),
+          showMap(),
+        ],
+      );
+  Set<Marker> shopMarker() {
+    return <Marker>[
+      Marker(
+          markerId: MarkerId('shopID'),
+          position: LatLng(
+            double.parse(userModel.lat),
+            double.parse(userModel.lng),
+          ),
+          infoWindow: InfoWindow(
+              title: 'ตำแหน่ง',
+              snippet:
+                  'ละติจูด = ${userModel.lat},ลองติจูด = ${userModel.lng}'))
+    ].toSet();
+  }
+
+  Widget showMap() {
+    double lat = double.parse(userModel.lat);
+    double lng = double.parse(userModel.lng);
+    LatLng latLng = LatLng(lat, lng);
+    CameraPosition position = CameraPosition(target: latLng, zoom: 16.0);
+    return Container(
+      padding: EdgeInsets.all(10.0),
+      height: 300.0,
+      child: GoogleMap(
+        initialCameraPosition: position,
+        mapType: MapType.normal,
+        onMapCreated: (controller) {},
+        markers: shopMarker(),
+      ),
+    );
+  }
+
+  Widget showNoData(BuildContext context) {
+    return MyStyle()
+        .titleCenter(context, 'ยังไม่มีข้อมูล กรุณาเพิ่มข้อมูลด้วยค่ะ');
   }
 
   Row addAnEditButton() {
